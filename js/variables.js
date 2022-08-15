6230f9ee47b1cc6bf42e9f8eaf8fb83131fd775b
Senu14 <makli203090@gmail.com>
2022-08-15 13:39:52 +0200

    Dom Markup

--- js/variables.js
+++ js/variables.js
@@ -11,6 +11,20 @@ const i = function () { };
 const j = new Date();
 
 
+console.log("varible a er " + typeof (a));
+console.log("varible a er " + typeof (b));
+console.log("varible a er " + typeof (c));
+console.log("varible a er " + typeof (d));
+console.log("varible a er " + typeof (e));
+console.log("varible a er " + typeof (f));
+console.log("varible a er " + typeof (g));
+console.log("varible a er " + typeof (h));
+console.log("varible a er " + typeof (i));
+console.log("varible a er " + typeof (j));
+
+
+
+
 
 
 
