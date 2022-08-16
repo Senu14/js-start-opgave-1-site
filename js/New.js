bc25c3f4817f45aee620c1ad62beeafef09cb9c1
Senu14 <makli203090@gmail.com>
2022-08-16 13:52:48 +0200

    Dom markup1

---
+++ js/New.js
@@ -0,0 +1,24 @@
+//   afsnit skift baggrund Farve   //
+const p = document.getElementById("p-id")
+p.style.backgroundColor = "#D5D5D5"
+
+//   fieldset skift baggrund Farve   //
+const fieldset = document.getElementById("fieldset-id")
+fieldset.style.backgroundColor = "#ddf"
+//   this is other way of Dom Traver   //
+function changeColor(element) {
+element.style.backgroundColor("#ddf")   
+}
+
+const input = document.getElementById("input-id")
+input.style.backgroundColor = "#ddd7db"
+
+const input1 = document.getElementById("input1-id")
+input1.style.backgroundColor = "#61636d"
+
+const input2 = document.getElementById("input2-id")
+input2.style.backgroundColor = "#a49295"
+
+// const h1h2 = Array.from(document.getElementsByClassName("h1,h2"))
+// h1,h2.style.backgroundcolor = "#864"
+
